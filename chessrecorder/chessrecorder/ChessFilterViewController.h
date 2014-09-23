264daa806bbd8404b805c54e5f3884a372c4e7ab
colman marcus-quinn <colman01@gmail.com>
2014-09-23 12:49:56 +0200

    incoming filter

---
+++ chessrecorder/chessrecorder/ChessFilterViewController.h
@@ -0,0 +1,39 @@
+//
+//  ChessFilterViewController.h
+//  chessrecorder
+//
+//  Created by colman on 23/09/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GPUImage/GPUImage.h>
+
+@interface ChessFilterViewController : UIViewController<GPUImageVideoCameraDelegate> {
+    GPUImageVideoCamera *videoCamera;
+    GPUImageOutput<GPUImageInput> *filter;
+    GPUImagePicture *sourcePicture;
+//    GPUImageShowcaseFilterType filterType;
+    GPUImageUIElement *uiElementInput;
+    IBOutlet GPUImageView *view1, *view2, *view3, *view4;
+    
+    GPUImageFilterPipeline *pipeline;
+    UIView *faceView;
+    
+    __unsafe_unretained UISlider *_filterSettingsSlider;
+}
+
+@property(readwrite, unsafe_unretained, nonatomic) IBOutlet UISlider *filterSettingsSlider;
+
+- (void)setupFilter;
+- (void)willOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer;
+// Filter adjustments
+- (IBAction)updateFilterFromSlider:(id)sender;
+- (void)GPUVCWillOutputFeatures:(NSArray*)featureArray forClap:(CGRect)clap
+                 andOrientation:(UIDeviceOrientation)curDeviceOrientation;
+
+
+@property IBOutlet GPUImageView *gpuImageView;
+
+
+@end
