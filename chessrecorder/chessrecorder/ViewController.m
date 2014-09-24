4c9fd52b00041746759214797bd077c65cf89e11
colman marcus-quinn <colman01@gmail.com>
2014-09-24 13:33:31 +0200

    Imaged rotated and transformed.

--- chessrecorder/chessrecorder/ViewController.m
+++ chessrecorder/chessrecorder/ViewController.m
@@ -24,8 +24,8 @@
 
 - (void) warpSpeedImage {
     
-    DHWarpView *warpView = [[DHWarpView alloc] initWithFrame:CGRectMake(0, 0, 3500, 3500)];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"chess.jpg"]];
+    DHWarpView *warpView = [[DHWarpView alloc] initWithFrame:CGRectMake(0, 0, 1400, 1400)];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"chess_rotate.jpg"]];
     
     warpView.frame = self.display.bounds;
     warpView.contentMode = UIViewContentModeScaleAspectFit; // or other desired mode
@@ -51,13 +51,23 @@
     //    warpView.bottomLeft = CGPointMake(350, 1000);
     //    warpView.bottomRight = CGPointMake(490, 540);
     
-    warpView.topLeft = CGPointMake(-20, -30);
-    warpView.topRight = CGPointMake(680, -30);
-    warpView.bottomLeft = CGPointMake(20, 550);
-    warpView.bottomRight = CGPointMake(470, 550);
+//    warpView.topLeft = CGPointMake(-20, -30);
+//    warpView.topRight = CGPointMake(680, -30);
+//    warpView.bottomLeft = CGPointMake(20, 550);
+//    warpView.bottomRight = CGPointMake(470, 550);
+    
+//    warpView.topLeft = CGPointMake(470, 550);
+//    warpView.topRight = CGPointMake(20, 550);
+//    warpView.bottomLeft = CGPointMake(680, -30);
+//    warpView.bottomRight = CGPointMake(-20, -30);
+    
+    warpView.topLeft = CGPointMake(27, 26);
+    warpView.topRight = CGPointMake(550, -30);
+    warpView.bottomLeft = CGPointMake(20, 400);
+    warpView.bottomRight = CGPointMake(555, 700);
     
     CGRect center = self.display.frame;
-    center.origin.x += 50;
+    center.origin.x -= 50;
     center.origin.y -= 400;
     [warpView setFrame:center];
     
