0ced3442182b6019fe3c20635932aa336f147325
colman marcus-quinn <colman01@gmail.com>
2014-09-25 15:34:56 +0200

    Chess model file to be added.

---
+++ chessrecorder/chessrecorder/ChessModel.h
@@ -0,0 +1,13 @@
+//
+//  ChessModel.h
+//  chessrecorder
+//
+//  Created by colman on 25/09/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChessModel : NSObject
+
+@end
---
+++ chessrecorder/chessrecorder/ChessModel.m
@@ -0,0 +1,13 @@
+//
+//  ChessModel.m
+//  chessrecorder
+//
+//  Created by colman on 25/09/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import "ChessModel.h"
+
+@implementation ChessModel
+
+@end
