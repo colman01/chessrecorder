7bba5df295918e09ed2a2b6d1657eeef12cc5109
colman marcus-quinn <colman01@gmail.com>
2014-09-29 21:31:05 +0200

    Final commit

--- chessrecorder/chessrecorder/OpenCVViewController.mm
+++ chessrecorder/chessrecorder/OpenCVViewController.mm
@@ -32,216 +32,19 @@
 - (void) viewDidAppear:(BOOL)animated {
 
     
-    GPUImageGaussianBlurFilter *gaussian = [[GPUImageGaussianBlurFilter alloc] init];
-    gaussian.blurRadiusInPixels = 5.0;
-    
-    GPUImage3x3ConvolutionFilter *derivativeXConv = [[GPUImage3x3ConvolutionFilter alloc] init];
-    [(GPUImage3x3ConvolutionFilter *)derivativeXConv setConvolutionKernel:(GPUMatrix3x3){
-        {1.0f,  0.0f, -1.0f},
-        {1.0f, 0.0f, -1.0f},
-        {1.0f,  0.0f, -1.0f}
-    }];
-    
-    GPUImage3x3ConvolutionFilter *derivativeYConv = [[GPUImage3x3ConvolutionFilter alloc] init];
-    [(GPUImage3x3ConvolutionFilter *)derivativeYConv setConvolutionKernel:(GPUMatrix3x3){
-        {1.0f,  1.0f, 1.0f},
-        {0.0f, 0.0f, 0.0f},
-        {-1.0f,  -1.0f, -1.0f}
-    }];
-    
-    GPUImage3x3ConvolutionFilter *cosConv = [[GPUImage3x3ConvolutionFilter alloc] init];
-    [(GPUImage3x3ConvolutionFilter *)cosConv setConvolutionKernel:(GPUMatrix3x3){
-        {cosf(M_PI_4),  0.0f, 0.0f},
-        {0.0f, cosf(M_PI_4), 0.0f},
-        {0.0f,  0.0f, cosf(M_PI_4)}
-    }];
-    
-    GPUImage3x3ConvolutionFilter *negCosConv = [[GPUImage3x3ConvolutionFilter alloc] init];
-    [(GPUImage3x3ConvolutionFilter *)negCosConv setConvolutionKernel:(GPUMatrix3x3){
-        {-cosf(M_PI_4),  0.0f, 0.0f},
-        {0.0f, -cosf(M_PI_4), 0.0f},
-        {0.0f,  0.0f, -cosf(M_PI_4)}
-    }];
-    
-    GPUImage3x3ConvolutionFilter *sinConv = [[GPUImage3x3ConvolutionFilter alloc] init];
-    [(GPUImage3x3ConvolutionFilter *)sinConv setConvolutionKernel:(GPUMatrix3x3){
-        {sinf(M_PI_4),  0.0f, 0.0f},
-        {0.0f, sinf(M_PI_4), 0.0f},
-        {0.0f,  0.0f, sinf(M_PI_4)}
-    }];
-    
-    GPUImage3x3ConvolutionFilter *negSinConv = [[GPUImage3x3ConvolutionFilter alloc] init];
-    [(GPUImage3x3ConvolutionFilter *)negSinConv setConvolutionKernel:(GPUMatrix3x3){
-        {-sinf(M_PI_4),  0.0f, 0.0f},
-        {0.0f, -sinf(M_PI_4), 0.0f},
-        {0.0f,  0.0f, -sinf(M_PI_4)}
-    }];
-    
-    UIImage *outputImage  = [[UIImage alloc] init];
-    
-    cv::Mat image1, image2, image3, image4;
-    
-    UIImage *img = [UIImage imageNamed:@"nicely.jpg"];
-    
-    
-    
-//    UIImage *img = [UIImage imageNamed:@"Chess_table.jpg"];
-    
-    cv::Mat grayImage = [CvMatUIImageConverter cvMatGrayFromUIImage:img];
-    
-    img = [CvMatUIImageConverter UIImageFromCVMat:grayImage];
+    [self performSelectorInBackground:@selector(createImages) withObject:nil];
+    [self performSelectorInBackground:@selector(calculate) withObject:nil];
 
     
-    UIImage *gaussianImage = [gaussian imageByFilteringImage:img];
-    UIImage *Ix = [derivativeXConv imageByFilteringImage:gaussianImage];
-    UIImage *Iy = [derivativeYConv imageByFilteringImage:gaussianImage];
     
-    
-    //    UIImage *Ix_45_cos = [cosConv imageByFilteringImage:Ix];
-    //    UIImage *Iy_45_sin = [sinConv imageByFilteringImage:Iy];
-    
-    
-    //    cv::Mat cvResult = [CvMatUIImageConverter cvMatGrayFromImage:outputImage];
-    cv::Mat cvResult = [CvMatUIImageConverter cvMatGrayFromUIImage:outputImage];
-    //    cv::Mat cvIx_cos = [CvMatUIImageConverter cvMatGrayFromUIImage:Ix_45_cos];
-    //    cv::Mat cvIy_sin = [CvMatUIImageConverter cvMatGrayFromUIImage:Iy_45_sin];
-    
-    cv::Mat cv_Ix_ = [CvMatUIImageConverter cvMatFromUIImage:gaussianImage];
-    cv::Mat cv_Iy_ = [CvMatUIImageConverter cvMatFromUIImage:gaussianImage];
-    cv::Mat cvIx_cos = cv_Ix_*cosf(M_PI_4);
-    cv::Mat cvIy_sin = cv_Iy_*-sinf(M_PI_4);
-    cv::Mat cv_I_45;
-    cv::add(cvIx_cos, cvIy_sin, cv_I_45);
-    
-    UIImage *I_45 = [CvMatUIImageConverter UIImageFromCVMat:cv_I_45];
-    UIImage *Ixy = [derivativeYConv imageByFilteringImage:I_45];
-    //    UIImage *Ixy = [xyFilter imageByFilteringImage:I_45];
-    
-    UIImage *I_45_x = [derivativeXConv imageByFilteringImage:I_45];
-    UIImage *I_45_y = [derivativeYConv imageByFilteringImage:I_45];
-    
-    cv::Mat cv_I_45_x = [CvMatUIImageConverter cvMatFromUIImage:I_45_x ]  ;
-    cv::Mat cv_I_45_y = [CvMatUIImageConverter cvMatFromUIImage:I_45_y ]  ;
-                       cv_I_45_x *= cosf(-M_PI_4);
-                       cv_I_45_y*= sinf(M_PI_4);
-                       cv::Mat cv_I_45_45;
-                       cv::add(cv_I_45_x, cv_I_45_y, cv_I_45_45);
-    
-    //    I_45_x * cos(-pi/4)
-    //    I_45_y * sin(-pi/4)
-//        UIImage *I_45_x_cos_n_pi4 = [negCosConv imageByFilteringImage:I_45_x];
-    //    UIImage *I_45_y_sin_n_pi4 = [negSinConv imageByFilteringImage:I_45_y];
-    
-    
-    cv::Mat posCos = cv_Ix_*cosf(M_PI_4);
-    cv::Mat posSin = cv_Iy_*sinf(M_PI_4);
-    
-    cv::Mat negCos = cv_Ix_*cosf(-M_PI_4);
-    cv::Mat negSin = cv_Iy_*sinf(-M_PI_4);
-
-//    UIImage *I_45_x_cos_n_pi4 = [CvMatUIImageConverter UIImageFromCVMat:negCos];
-    UIImage *I_45_y_sin_n_pi4 = [CvMatUIImageConverter UIImageFromCVMat:negSin];
-    
-    
-    
-    
-    cv::Mat cv_Ixy = [CvMatUIImageConverter cvMatFromUIImage:Ixy];
-//    cv::Mat cv_I_45_x_cos_n_pi4 = [CvMatUIImageConverter cvMatFromUIImage:I_45_x_cos_n_pi4];
-    cv::Mat cv_I_45_x_cos_n_pi4 = negCos;
-    cv::Mat cv_I_45_y_sin_n_pi4 = [CvMatUIImageConverter cvMatFromUIImage:I_45_y_sin_n_pi4];
-    
-    //    % first derivative at 45 degrees
-    //    I_45 = Ix * cos(pi/4) + Iy * sin(pi/4);
-    //    I_n45 = Ix * cos(-pi/4) + Iy * sin(-pi/4);
-    
-    cv::Mat cv_Ix_45;
-    cv::Mat cv_Iy_45;
-//    cv::Mat cv_I_45;
-    cv::add(posCos, posSin, cv_I_45);
-    
-    cv::Mat cv_I_n45;
-    cv::add(negCos, negSin, cv_I_n45);
-    
-    //    I_45_45 = I_45_x * cos(-pi/4) + I_45_y * sin(-pi/4);
-    cv::Mat negSum;
-    
-//    cv::add(negCos, cv_I_n45, negSum);
-
-    cv::Mat cv_cos_n45 = cosf(-M_PI_4)*negCos;
-    cv::Mat cv_sin_n45 = sinf(-M_PI_4)*negSin;
-    cv::Mat Img_45_45;
-    cv::add(cv_cos_n45, cv_sin_n45, Img_45_45);
-    
-//    cv::add(negCos, cv_I_45_y_sin_n_pi4, negSum);
-    cvResult = Img_45_45;
-
-//    cv::add(cv_I_45_x_cos_n_pi4, cv_I_45_y_sin_n_pi4, negSum);
-//    cv::add(negCos, cv_I_45_y_sin_n_pi4, negSum);
-//    cvResult = I_45_45;
-
-    cv::Mat absIxy = cv::abs(cv_Ixy);
-    cv::Mat abs_I_45 = cv::abs(cv_I_45_x_cos_n_pi4);
-    cv::Mat abs_I_n45 = cv::abs(cv_I_45_y_sin_n_pi4);
-    
-    cv::Mat LHS ;
-    cv::add(abs_I_45, abs_I_n45, LHS);
-    LHS *= 2;
-    cv::Mat RHS = 4*absIxy;
-    cv::Mat I_n45;
-    cv::subtract(RHS, LHS, I_n45);
-    // I_n45
-    // image1 = I_n45;
-    
-//    cv::Mat mask = I_n45 <= 0 ;
-//    cv::Mat masked = cvResult.setTo(0, cvResult<0 );
-//    I_n45 = I_n45.setTo(0, I_n45<= 0);
-    cv::Mat mask = (I_n45 <= 0);
-    
-    cv::Mat mask_2 = (cv_I_45_45 <= 0);
-
-    cv::Mat nonZeroLocations;
-    //    UIImage *cvOutput = [CvMatUIImageConverter UIImageFromCVMat:masked];
-    //    c45 = sigma^2 * abs(I_45_45) - sigma * (abs(Ix) + abs(Iy));
-//    cv::Mat absI4545 = cv::abs(cv_I_45_45);
-//    cv::Mat absI4545 = cv::abs(Img_45_45);
-//    cv::Mat sigmaAbsI4545 = 4*absI4545;
-    cv::Mat sigmaAbsI4545 = 4*mask;
-    cv::Mat cv_Ix = [CvMatUIImageConverter cvMatFromUIImage:Ix];
-    cv::Mat cv_Iy = [CvMatUIImageConverter cvMatFromUIImage:Iy];
-    cv::Mat absIx, absIy;
-    absIx = cv::abs(cv_Ix);
-    absIy = cv::abs(cv_Iy);
-    cv::add(absIx, absIy, LHS);
-    LHS *= 2;
-//    RHS = 4*absI4545;
-        RHS = 4*mask;
-    cv::Mat cv_c45;
-    cv::subtract(RHS, LHS, cv_c45);
-    
-    image1 = cvResult;
-    image2 = cv_c45;
-//    UIImage *cvOutput = [CvMatUIImageConverter UIImageFromCVMat:cv_c45];
-    [imageView setImage:Ix];
-//    [imageViewC45 setImage:Iy];
-    [imageViewC45 setImage:[CvMatUIImageConverter UIImageFromCVMat:mask_2]];
-//    [imageViewCxy setImage:[CvMatUIImageConverter UIImageFromCVMat:Img_45_45]]; //cv_I_n45
-    [imageViewCxy setImage:[CvMatUIImageConverter UIImageFromCVMat:mask]]; //cv_I_n45
-    [imageViewI setImage:[CvMatUIImageConverter UIImageFromCVMat:cv_I_45_45]]; //I_n45
-//    [imageViewI setImage:[CvMatUIImageConverter UIImageFromCVMat:cv_c45]]; //I_n45
-                       
-//    [imageViewI setImage:cvOutput];
-    
-    [imageViewI setImage:[self matlab_I_cxy:img]];
 }
 
-
-
-
-
-
-
-
-
+- (void) calculate {
+    UIImage *img = [UIImage imageNamed:@"nicely.jpg"];
+    cv::Mat grayImage = [CvMatUIImageConverter cvMatGrayFromUIImage:img];
+    img = [CvMatUIImageConverter UIImageFromCVMat:grayImage];
+    [imageViewI setImage:[self matlab_I_cxy:img]];
+}
 
 - (UIImage *) matlab_I_cxy:(UIImage *) image {
     GPUImageGaussianBlurFilter *gaussianFilter = [[GPUImageGaussianBlurFilter alloc ] init];
@@ -326,6 +129,149 @@
     
 }
 
+
+- (void) createImages {
+    UIImage *img = [UIImage imageNamed:@"nicely.jpg"];
+    cv::Mat grayImage = [CvMatUIImageConverter cvMatGrayFromUIImage:img];
+    img = [CvMatUIImageConverter UIImageFromCVMat:grayImage];
+    
+    UIImage *outputImage  = [[UIImage alloc] init];
+    cv::Mat image1, image2, image3, image4;
+    
+    GPUImageGaussianBlurFilter *gaussian = [[GPUImageGaussianBlurFilter alloc] init];
+    gaussian.blurRadiusInPixels = 5.0;
+    
+    GPUImage3x3ConvolutionFilter *derivativeXConv = [[GPUImage3x3ConvolutionFilter alloc] init];
+    [(GPUImage3x3ConvolutionFilter *)derivativeXConv setConvolutionKernel:(GPUMatrix3x3){
+        {1.0f,  0.0f, -1.0f},
+        {1.0f, 0.0f, -1.0f},
+        {1.0f,  0.0f, -1.0f}
+    }];
+    
+    GPUImage3x3ConvolutionFilter *derivativeYConv = [[GPUImage3x3ConvolutionFilter alloc] init];
+    [(GPUImage3x3ConvolutionFilter *)derivativeYConv setConvolutionKernel:(GPUMatrix3x3){
+        {1.0f,  1.0f, 1.0f},
+        {0.0f, 0.0f, 0.0f},
+        {-1.0f,  -1.0f, -1.0f}
+    }];
+    
+    GPUImage3x3ConvolutionFilter *cosConv = [[GPUImage3x3ConvolutionFilter alloc] init];
+    [(GPUImage3x3ConvolutionFilter *)cosConv setConvolutionKernel:(GPUMatrix3x3){
+        {cosf(M_PI_4),  0.0f, 0.0f},
+        {0.0f, cosf(M_PI_4), 0.0f},
+        {0.0f,  0.0f, cosf(M_PI_4)}
+    }];
+    
+    GPUImage3x3ConvolutionFilter *negCosConv = [[GPUImage3x3ConvolutionFilter alloc] init];
+    [(GPUImage3x3ConvolutionFilter *)negCosConv setConvolutionKernel:(GPUMatrix3x3){
+        {-cosf(M_PI_4),  0.0f, 0.0f},
+        {0.0f, -cosf(M_PI_4), 0.0f},
+        {0.0f,  0.0f, -cosf(M_PI_4)}
+    }];
+    
+    GPUImage3x3ConvolutionFilter *sinConv = [[GPUImage3x3ConvolutionFilter alloc] init];
+    [(GPUImage3x3ConvolutionFilter *)sinConv setConvolutionKernel:(GPUMatrix3x3){
+        {sinf(M_PI_4),  0.0f, 0.0f},
+        {0.0f, sinf(M_PI_4), 0.0f},
+        {0.0f,  0.0f, sinf(M_PI_4)}
+    }];
+    
+    GPUImage3x3ConvolutionFilter *negSinConv = [[GPUImage3x3ConvolutionFilter alloc] init];
+    [(GPUImage3x3ConvolutionFilter *)negSinConv setConvolutionKernel:(GPUMatrix3x3){
+        {-sinf(M_PI_4),  0.0f, 0.0f},
+        {0.0f, -sinf(M_PI_4), 0.0f},
+        {0.0f,  0.0f, -sinf(M_PI_4)}
+    }];
+    
+    
+    UIImage *gaussianImage = [gaussian imageByFilteringImage:img];
+    UIImage *Ix = [derivativeXConv imageByFilteringImage:gaussianImage];
+    UIImage *Iy = [derivativeYConv imageByFilteringImage:gaussianImage];
+    
+    cv::Mat cvResult = [CvMatUIImageConverter cvMatGrayFromUIImage:outputImage];
+    
+    cv::Mat cv_Ix_ = [CvMatUIImageConverter cvMatFromUIImage:gaussianImage];
+    cv::Mat cv_Iy_ = [CvMatUIImageConverter cvMatFromUIImage:gaussianImage];
+    cv::Mat cvIx_cos = cv_Ix_*cosf(M_PI_4);
+    cv::Mat cvIy_sin = cv_Iy_*-sinf(M_PI_4);
+    cv::Mat cv_I_45;
+    cv::add(cvIx_cos, cvIy_sin, cv_I_45);
+    
+    UIImage *I_45 = [CvMatUIImageConverter UIImageFromCVMat:cv_I_45];
+    UIImage *Ixy = [derivativeYConv imageByFilteringImage:I_45];
+    //    UIImage *Ixy = [xyFilter imageByFilteringImage:I_45];
+    
+    UIImage *I_45_x = [derivativeXConv imageByFilteringImage:I_45];
+    UIImage *I_45_y = [derivativeYConv imageByFilteringImage:I_45];
+    
+    cv::Mat cv_I_45_x = [CvMatUIImageConverter cvMatFromUIImage:I_45_x ]  ;
+    cv::Mat cv_I_45_y = [CvMatUIImageConverter cvMatFromUIImage:I_45_y ]  ;
+    cv_I_45_x *= cosf(-M_PI_4);
+    cv_I_45_y*= sinf(M_PI_4);
+    cv::Mat cv_I_45_45;
+    cv::add(cv_I_45_x, cv_I_45_y, cv_I_45_45);
+    
+    cv::Mat posCos = cv_Ix_*cosf(M_PI_4);
+    cv::Mat posSin = cv_Iy_*sinf(M_PI_4);
+    
+    cv::Mat negCos = cv_Ix_*cosf(-M_PI_4);
+    cv::Mat negSin = cv_Iy_*sinf(-M_PI_4);
+    
+    UIImage *I_45_y_sin_n_pi4 = [CvMatUIImageConverter UIImageFromCVMat:negSin];
+    
+    cv::Mat cv_Ixy = [CvMatUIImageConverter cvMatFromUIImage:Ixy];
+    cv::Mat cv_I_45_x_cos_n_pi4 = negCos;
+    cv::Mat cv_I_45_y_sin_n_pi4 = [CvMatUIImageConverter cvMatFromUIImage:I_45_y_sin_n_pi4];
+    cv::Mat cv_Ix_45;
+    cv::Mat cv_Iy_45;
+    cv::add(posCos, posSin, cv_I_45);
+    
+    cv::Mat cv_I_n45;
+    cv::add(negCos, negSin, cv_I_n45);
+    
+    //    I_45_45 = I_45_x * cos(-pi/4) + I_45_y * sin(-pi/4);
+    cv::Mat negSum;
+    cv::Mat cv_cos_n45 = cosf(-M_PI_4)*negCos;
+    cv::Mat cv_sin_n45 = sinf(-M_PI_4)*negSin;
+    cv::Mat Img_45_45;
+    cv::add(cv_cos_n45, cv_sin_n45, Img_45_45);
+    
+    //    cv::add(negCos, cv_I_45_y_sin_n_pi4, negSum);
+    cvResult = Img_45_45;
+    cv::Mat absIxy = cv::abs(cv_Ixy);
+    cv::Mat abs_I_45 = cv::abs(cv_I_45_x_cos_n_pi4);
+    cv::Mat abs_I_n45 = cv::abs(cv_I_45_y_sin_n_pi4);
+    
+    cv::Mat LHS ;
+    cv::add(abs_I_45, abs_I_n45, LHS);
+    LHS *= 2;
+    cv::Mat RHS = 4*absIxy;
+    cv::Mat I_n45;
+    cv::subtract(RHS, LHS, I_n45);
+    cv::Mat mask = (I_n45 <= 0);
+    
+    cv::Mat mask_2 = (cv_I_45_45 <= 0);
+    
+    cv::Mat sigmaAbsI4545 = 4*mask;
+    cv::Mat cv_Ix = [CvMatUIImageConverter cvMatFromUIImage:Ix];
+    cv::Mat cv_Iy = [CvMatUIImageConverter cvMatFromUIImage:Iy];
+    cv::Mat absIx, absIy;
+    absIx = cv::abs(cv_Ix);
+    absIy = cv::abs(cv_Iy);
+    cv::add(absIx, absIy, LHS);
+    LHS *= 2;
+    RHS = 4*mask;
+    cv::Mat cv_c45;
+    cv::subtract(RHS, LHS, cv_c45);
+    
+    image1 = cvResult;
+    image2 = cv_c45;
+    [imageView setImage:Ix];
+    [imageViewC45 setImage:[CvMatUIImageConverter UIImageFromCVMat:mask_2]];
+    [imageViewCxy setImage:[CvMatUIImageConverter UIImageFromCVMat:mask]]; //cv_I_n45
+    [imageViewI setImage:[CvMatUIImageConverter UIImageFromCVMat:cv_I_45_45]]; //I_n45
+
+}
 
 
 
