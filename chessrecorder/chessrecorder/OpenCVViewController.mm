910128395e2d9f6b5c8792348ca1e3d72b4d01e1
colman marcus-quinn <colman01@gmail.com>
2014-09-29 18:35:28 +0200

    Changing of variable.

--- chessrecorder/chessrecorder/OpenCVViewController.mm
+++ chessrecorder/chessrecorder/OpenCVViewController.mm
@@ -105,16 +105,22 @@
     cv::Mat cv_Iy_ = [CvMatUIImageConverter cvMatFromUIImage:gaussianImage];
     cv::Mat cvIx_cos = cv_Ix_*cosf(M_PI_4);
     cv::Mat cvIy_sin = cv_Iy_*-sinf(M_PI_4);
+    cv::Mat cv_I_45;
+    cv::add(cvIx_cos, cvIy_sin, cv_I_45);
     
-    cv::add(cvIx_cos, cvIy_sin, cvResult);
-    
-    UIImage *I_45 = [CvMatUIImageConverter UIImageFromCVMat:cvResult];
+    UIImage *I_45 = [CvMatUIImageConverter UIImageFromCVMat:cv_I_45];
     UIImage *Ixy = [derivativeYConv imageByFilteringImage:I_45];
     //    UIImage *Ixy = [xyFilter imageByFilteringImage:I_45];
     
-        UIImage *I_45_x = [derivativeXConv imageByFilteringImage:I_45];
-    //    UIImage *I_45_y = [derivativeYConv imageByFilteringImage:I_45];
+    UIImage *I_45_x = [derivativeXConv imageByFilteringImage:I_45];
+    UIImage *I_45_y = [derivativeYConv imageByFilteringImage:I_45];
     
+    cv::Mat cv_I_45_x = [CvMatUIImageConverter cvMatFromUIImage:I_45_x ]  ;
+    cv::Mat cv_I_45_y = [CvMatUIImageConverter cvMatFromUIImage:I_45_y ]  ;
+                       cv_I_45_x *= cosf(-M_PI_4);
+                       cv_I_45_y*= sinf(M_PI_4);
+                       cv::Mat cv_I_45_45;
+                       cv::add(cv_I_45_x, cv_I_45_y, cv_I_45_45);
     
     //    I_45_x * cos(-pi/4)
     //    I_45_y * sin(-pi/4)
@@ -145,7 +151,7 @@
     
     cv::Mat cv_Ix_45;
     cv::Mat cv_Iy_45;
-    cv::Mat cv_I_45;
+//    cv::Mat cv_I_45;
     cv::add(posCos, posSin, cv_I_45);
     
     cv::Mat cv_I_n45;
@@ -184,28 +190,17 @@
 //    cv::Mat mask = I_n45 <= 0 ;
 //    cv::Mat masked = cvResult.setTo(0, cvResult<0 );
 //    I_n45 = I_n45.setTo(0, I_n45<= 0);
-    
     cv::Mat mask = (I_n45 <= 0);
+    
+    cv::Mat mask_2 = (cv_I_45_45 <= 0);
 
- 
-//    I_n45 = I_n45.setTo(0.5,mask);
     cv::Mat nonZeroLocations;
-//    cv::findNonZero(I_n45, nonZeroLocations);
-//    cv::threshold(I_n45, nonZeroLocations, 0, 1, cv::THRESH_MASK);
-    
-    
-    //    cv::Mat mask = cvResult <= 0 ;
-    //    cvResult.setTo(mask);
-    //    cv::Mat masked = cvResult.setTo(0, cvResult<0 );
-    
-    //    Mat im = ReadSomeImage(...);
-    //    Mat masked = im.setTo(0,im<0); /// <<<
-    
     //    UIImage *cvOutput = [CvMatUIImageConverter UIImageFromCVMat:masked];
     //    c45 = sigma^2 * abs(I_45_45) - sigma * (abs(Ix) + abs(Iy));
 //    cv::Mat absI4545 = cv::abs(cv_I_45_45);
-    cv::Mat absI4545 = cv::abs(Img_45_45);
-    cv::Mat sigmaAbsI4545 = 4*absI4545;
+//    cv::Mat absI4545 = cv::abs(Img_45_45);
+//    cv::Mat sigmaAbsI4545 = 4*absI4545;
+    cv::Mat sigmaAbsI4545 = 4*mask;
     cv::Mat cv_Ix = [CvMatUIImageConverter cvMatFromUIImage:Ix];
     cv::Mat cv_Iy = [CvMatUIImageConverter cvMatFromUIImage:Iy];
     cv::Mat absIx, absIy;
@@ -213,7 +208,8 @@
     absIy = cv::abs(cv_Iy);
     cv::add(absIx, absIy, LHS);
     LHS *= 2;
-    RHS = 4*absI4545;
+//    RHS = 4*absI4545;
+        RHS = 4*mask;
     cv::Mat cv_c45;
     cv::subtract(RHS, LHS, cv_c45);
     
@@ -221,35 +217,17 @@
     image2 = cv_c45;
 //    UIImage *cvOutput = [CvMatUIImageConverter UIImageFromCVMat:cv_c45];
     [imageView setImage:Ix];
-    [imageViewC45 setImage:Iy];
+//    [imageViewC45 setImage:Iy];
+    [imageViewC45 setImage:[CvMatUIImageConverter UIImageFromCVMat:mask_2]];
 //    [imageViewCxy setImage:[CvMatUIImageConverter UIImageFromCVMat:Img_45_45]]; //cv_I_n45
     [imageViewCxy setImage:[CvMatUIImageConverter UIImageFromCVMat:mask]]; //cv_I_n45
-    [imageViewI setImage:[CvMatUIImageConverter UIImageFromCVMat:cv_c45]]; //I_n45
+    [imageViewI setImage:[CvMatUIImageConverter UIImageFromCVMat:cv_I_45_45]]; //I_n45
+//    [imageViewI setImage:[CvMatUIImageConverter UIImageFromCVMat:cv_c45]]; //I_n45
+                       
 //    [imageViewI setImage:cvOutput];
 }
 
-//- (void) returnNoZero  {
-////    cv::Mat m = (cv::Mat)n;
-////
-//////    cv::Mat someMat(1, 4, CV_64F, &someData);;
-////    cv::Matiterator_<double> _it = m.begin<double>();
-////    for(;_it!=m.end<double>(); _it++){
-////        std::cout << *_it << std::endl;
-////    }
-//    cv::Mat m;
-//    for(int i = 0; i < m.rows; i++) {
-////        double* mi = m.at<cvScalar>(i);
-//        double* mi = m.ptr<CvScalar>(i);
-//        
-//        
-//        for(int j = 0; j < m.cols; j++) {
-//            if (mi[j] < 0) {
-//                printf("changing [%i, %i] from %8.1f to 0\n", i, j, mi[j]);
-//                mi[j] = cvScalar(0,0,0);
-//            }
-//        }
-//    }
-//}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
