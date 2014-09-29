662464b2b38f5fdf5c9c7544e940eec8a0932aca
Daniel Strecker <daniel.strecker@gmx.net>
2014-09-29 18:24:25 +0200

    added black-white-field recognition

--- chessrecorder/chessrecorder/HomographyVc.mm
+++ chessrecorder/chessrecorder/HomographyVc.mm
@@ -31,7 +31,7 @@
     cv::Point2f* src = (cv::Point2f*) malloc(4 * sizeof(cv::Point2f));
     cv::Point2f* dst = (cv::Point2f*) malloc(4 * sizeof(cv::Point2f));
     
-    switch (rand() % 4) {
+    switch (rand() % 5) {
         case 0:
             srcImgUi = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"chessboard_03" ofType:@"jpg"]];
             src[0] = cv::Point2f(81, 308);
@@ -48,10 +48,10 @@
             break;
         case 2:
             srcImgUi = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"chess_rotate" ofType:@"jpg"]];
-            src[0] = cv::Point2f(521, 141);
-            src[1] = cv::Point2f(519, 636);
-            src[2] = cv::Point2f( 81, 696);
-            src[3] = cv::Point2f( 80,  84);
+            src[0] = cv::Point2f( 80,  84);
+            src[1] = cv::Point2f(521, 141);
+            src[2] = cv::Point2f(519, 636);
+            src[3] = cv::Point2f( 81, 696);
             break;
         case 3:
             srcImgUi = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Chess_table" ofType:@"jpg"]];
@@ -59,6 +59,13 @@
             src[1] = cv::Point2f(549, 216);
             src[2] = cv::Point2f(652, 314);
             src[3] = cv::Point2f(404, 355);
+            break;
+        case 4:
+            srcImgUi = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Chess_board_top" ofType:@"jpg"]];
+            src[0] = cv::Point2f( 83,  96);
+            src[1] = cv::Point2f(708,  87);
+            src[2] = cv::Point2f(706, 715);
+            src[3] = cv::Point2f( 86, 711);
             break;
     }
 
@@ -89,18 +96,52 @@
     
     cv::Mat srcImg = [CvMatUIImageConverter cvMatFromUIImage:srcImgUi];
     
-    cv::Mat dstImg;
-    cv::warpPerspective(srcImg, dstImg, m, cv::Size(dstImgSize, dstImgSize));
+    cv::Mat plainBoardImg;
+    cv::warpPerspective(srcImg, plainBoardImg, m, cv::Size(dstImgSize, dstImgSize));
     
-    UIImage* dstImgUi = [CvMatUIImageConverter UIImageFromCVMat:dstImg];
+    cv::Mat sub = srcImg(cv::Rect(srcImg.cols - plainBoardImg.cols, 0, plainBoardImg.cols, plainBoardImg.rows));
+    plainBoardImg.copyTo(sub);
+
+    cv::Rect fieldRect = cv::Rect(0, 0, plainBoardImg.cols / 8, plainBoardImg.rows / 8);
+    cv::Mat fieldType0Mean = cv::Mat::zeros(fieldRect.height, fieldRect.width, CV_16UC4);
+    cv::Mat fieldType1Mean = cv::Mat::zeros(fieldRect.height, fieldRect.width, CV_16UC4);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            fieldRect.x = fieldRect.width * i;
+            fieldRect.y = fieldRect.height * j;
+            
+            cv::Mat field(plainBoardImg, fieldRect);
+            field.convertTo(field, CV_16UC4);
+            field /= 32;
+            
+            if ((i + j) % 2 == 0) {
+                fieldType0Mean += field;
+            } else {
+                fieldType1Mean += field;
+            }
+        }
+    }
     
-    UIGraphicsBeginImageContext(srcImgUi.size);
-    [srcImgUi drawAtPoint:CGPointZero];
-    [dstImgUi drawAtPoint:CGPointMake(srcImgUi.size.width - dstImgUi.size.width, 0)];
-    //CGContextRef context = UIGraphicsGetCurrentContext();
-    UIImage *combinedImg = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+    fieldRect.x = 0; fieldRect.y = 0;
+    fieldType0Mean.copyTo(srcImg(fieldRect));
+    fieldRect.x = fieldRect.width; fieldRect.y = 0;
+    fieldType1Mean.copyTo(srcImg(fieldRect));
     
+    cv::Scalar meanPixel = cv::mean(fieldType0Mean);
+    meanPixel.val[3] = 0;
+    double brightnessType0 = sqrtl(meanPixel.dot(meanPixel));
+    
+    meanPixel = cv::mean(fieldType1Mean);
+    meanPixel.val[3] = 0;
+    double brightnessType1 = sqrtl(meanPixel.dot(meanPixel));
+    
+    if (brightnessType0 < brightnessType1) {
+        printf("board is adjusted left-right, thus it needs to be rotated 90deg\n");
+    } else {
+        printf("board is adjusted top-bottom, thus there is no need to rotate it by 90deg\n");
+    }
+    
+    UIImage* combinedImg = [CvMatUIImageConverter UIImageFromCVMat:srcImg];
     self.imgView.image = combinedImg;
 }
 
