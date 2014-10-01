23efc22aaf1fd95f852e2e20c2208545a10fc8af
colman marcus-quinn <colman01@gmail.com>
2014-10-01 11:24:23 +0200

    Experimenting with new GUI.

---
+++ chessrecorder/chessrecorder/MainscreenViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainscreenViewController.m
+//  chessrecorder
+//
+//  Created by colman on 01/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import "MainscreenViewController.h"
+
+@interface MainscreenViewController ()
+
+@end
+
+@implementation MainscreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
