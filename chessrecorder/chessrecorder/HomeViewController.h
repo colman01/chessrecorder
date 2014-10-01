5a20abf36e7de44f43a70e09c7ea47d9bbfaa8b6
colman marcus-quinn <colman01@gmail.com>
2014-10-01 19:02:00 +0200

    Tab controllers continued. Basic functions and classes added for navigation.

---
+++ chessrecorder/chessrecorder/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  chessrecorder
+//
+//  Created by colman on 01/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+
+- (IBAction)goBack:(id)sender;
+
+@end
---
+++ chessrecorder/chessrecorder/HomeViewController.m
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.m
+//  chessrecorder
+//
+//  Created by colman on 01/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)goBack:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
