aa3618a70996ad742bddb61b6dce8c359115f62d
colman marcus-quinn <colman01@gmail.com>
2014-10-01 12:36:25 +0200

    Suggested layout cons added.

---
+++ chessrecorder/chessrecorder/GameHistoryViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameHistoryViewController.h
+//  chessrecorder
+//
+//  Created by colman on 01/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameHistoryViewController : UIViewController <UITableViewDelegate>
+
+
+@property IBOutlet UITableView *gameList;
+@end
---
+++ chessrecorder/chessrecorder/GameHistoryViewController.m
@@ -0,0 +1,103 @@
+//
+//  GameHistoryViewController.m
+//  chessrecorder
+//
+//  Created by colman on 01/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import "GameHistoryViewController.h"
+
+@interface GameHistoryViewController ()
+
+@end
+
+@implementation GameHistoryViewController
+
+@synthesize gameList;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.gameList.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+
+ - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+ UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GameCell" forIndexPath:indexPath];
+ 
+ 
+ return cell;
+ }
+
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
