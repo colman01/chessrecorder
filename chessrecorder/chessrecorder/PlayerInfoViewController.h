bcd47adae596483ae07c56eb0aa56a071807e641
colman marcus-quinn <colman01@gmail.com>
2014-10-02 16:50:51 +0200

    Info con added.

---
+++ chessrecorder/chessrecorder/PlayerInfoViewController.h
@@ -0,0 +1,24 @@
+//
+//  PlayerInfoViewController.h
+//  chessrecorder
+//
+//  Created by colman on 02/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerInfoViewController : UIViewController
+
+
+@property IBOutlet UITextView *event;
+@property IBOutlet UITextView *location;
+@property IBOutlet UITextView *date;
+@property IBOutlet UITextView *round;
+@property IBOutlet UITextView *whitePlayer;
+@property IBOutlet UITextView *blackPlayer;
+@property IBOutlet UITextView *result;
+
+
+
+@end
---
+++ chessrecorder/chessrecorder/PlayerInfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlayerInfoViewController.m
+//  chessrecorder
+//
+//  Created by colman on 02/10/14.
+//  Copyright (c) 2014 Colman Marcus-Quinn. All rights reserved.
+//
+
+#import "PlayerInfoViewController.h"
+
+@interface PlayerInfoViewController ()
+
+@end
+
+@implementation PlayerInfoViewController
+
+@synthesize event, location, date, round, whitePlayer, blackPlayer, result;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
